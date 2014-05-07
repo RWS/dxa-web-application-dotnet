ffcc90b6412ed2b4afcaed53acca0e963ad4e479
Bart Koopman <bkoopman@sdl.com>
2014-05-07 16:05:00 +0200

    added metadata field handling

--- Sdl.Web.DD4T/Mapping/DD4TEntityBuilder.cs
+++ Sdl.Web.DD4T/Mapping/DD4TEntityBuilder.cs
@@ -1,28 +1,26 @@
-﻿using DD4T.ContentModel;
-using Sdl.Web.Mvc;
-using Sdl.Web.Mvc.Mapping;
-using Sdl.Web.Mvc.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using DD4T.ContentModel;
+using Sdl.Web.Mvc.Mapping;
+using Sdl.Web.Mvc.Models;
 
 namespace Sdl.Web.DD4T.Mapping
 {
     public class DD4TEntityBuilder : BaseEntityBuilder
     {
         public ExtensionlessLinkFactory LinkFactory { get; set; }
+
         public DD4TEntityBuilder()
         {
-            this.LinkFactory = new ExtensionlessLinkFactory();
+            LinkFactory = new ExtensionlessLinkFactory();
         }
         
         public override object Create(object sourceEntity,Type type)
         {
             IComponent component = sourceEntity as IComponent;
-            Dictionary<string, string> entityData = null;
+            Dictionary<string, string> entityData;
             Dictionary<string, string> propertyData = new Dictionary<string, string>();
             if (component == null && sourceEntity is IComponentPresentation)
             {
@@ -41,19 +39,8 @@ namespace Sdl.Web.DD4T.Mapping
                 long schemaId = Convert.ToInt64(uriParts[1]);
 
                 // get schema entity names (indexed by vocabulary)
-                SemanticSchema semanticSchema = null;
-                ILookup<string, string> entityNames = null;
-                try
-                {
-                    // TODO make sure all schemas are available, so we can remove this try/catch
-                    semanticSchema = SemanticMapping.GetSchema(schemaId);
-                    entityNames = semanticSchema.GetEntityNames();
-                }
-                catch (Exception ex)
-                {
-                    // semantic schema is not there, we'll need to skip semantics
-                    Log.Error(ex);
-                }
+                SemanticSchema semanticSchema = SemanticMapping.GetSchema(schemaId);
+                ILookup<string, string> entityNames = semanticSchema.GetEntityNames();
 
                 var model = Activator.CreateInstance(type);
                 Dictionary<string, string> vocabularies = GetVocabulariesFromType(type);
@@ -79,17 +66,13 @@ namespace Sdl.Web.DD4T.Mapping
                             }
 
                             // determine field semantics
-                            SemanticSchemaField matchingField = null;
-                            if (semanticSchema != null)
-                            {
-                                string prefix = SemanticMapping.GetPrefix(vocab);
-                                string property = info.PropertyName;
-                                string entity = entityNames[vocab].First();
-                                FieldSemantics fieldSemantics = new FieldSemantics(prefix, entity, property);
+                            string prefix = SemanticMapping.GetPrefix(vocab);
+                            string property = info.PropertyName;
+                            string entity = entityNames[vocab].First();
+                            FieldSemantics fieldSemantics = new FieldSemantics(prefix, entity, property);
 
-                                // locate semantic schema field
-                                semanticSchema.FindFieldBySemantics(fieldSemantics);                                
-                            }
+                            // locate semantic schema field
+                            SemanticSchemaField matchingField = semanticSchema.FindFieldBySemantics(fieldSemantics);  
                             if (matchingField != null)
                             {
                                 // we found a field with given semantics
@@ -114,36 +97,44 @@ namespace Sdl.Web.DD4T.Mapping
                                 pi.SetValue(model, GetImages(new List<IComponent>{component})[0]);
                             }
 
-                            //TODO check metadata/embedded fields as well!
+                            // determine type
+                            Type propertyType = multival ? pi.PropertyType.GetGenericArguments()[0] : pi.PropertyType;
+
+                            // try getting value from content fields
                             if (component.Fields.ContainsKey(fieldname))
                             {
                                 var field = component.Fields[fieldname];
-                                Type propertyType = multival ? pi.PropertyType.GetGenericArguments()[0] : pi.PropertyType;
-                                switch (field.FieldType)
-                                {
-                                    case (FieldType.Date):
-                                        pi.SetValue(model, GetDates(field, propertyType, multival));
-                                        break;
-                                    case (FieldType.Number):
-                                        pi.SetValue(model, GetNumbers(field, propertyType, multival));
-                                        break;
-                                    case (FieldType.MultiMediaLink):
-                                        pi.SetValue(model, GetMultiMediaLinks(field, propertyType, multival));
-                                        break;
-                                    case (FieldType.ComponentLink):
-                                        pi.SetValue(model, GetMultiComponentLinks(field, propertyType, multival));
-                                        break;
-                                    case (FieldType.Embedded):
-                                        pi.SetValue(model, GetMultiEmbedded(field, propertyType, multival));
-                                        break;
-                                    default:
-                                        pi.SetValue(model, GetStrings(field, propertyType, multival));
-                                        break;
-                                }
-                                propertyData.Add(pi.Name,GetFieldXPath(field));
-                                //If we found a field, we are done - no need to process other semantics for this property
+                                pi.SetValue(model, GetFieldValues(field, propertyType, multival));
+                                propertyData.Add(pi.Name, GetFieldXPath(field));
+                                // we found a field, we are done - no need to process other semantics for this property
                                 break;
                             }
+
+                            // try getting value from metadata fields
+                            if (component.MetadataFields.ContainsKey(fieldname))
+                            {
+                                var field = component.MetadataFields[fieldname];
+                                pi.SetValue(model, GetFieldValues(field, propertyType, multival));
+                                propertyData.Add(pi.Name, GetFieldXPath(field));
+                                // we found a field, we are done - no need to process other semantics for this property
+                                break;
+                            }
+
+                            if (matchingField != null && matchingField.IsEmbedded)
+                            {
+                                // we are dealing with an embedded field
+                                // TODO get embedded value 
+
+
+                                if (matchingField.IsMetadata)
+                                {
+                                    // we are dealing with an embedded metadata field                                    
+                                    // TODO get embedded metadata value
+
+                                }
+                            }
+                            // TODO if semantic field could not be matched, this could still be an embedded (metadata) field
+
                         }
                     }
                 }
@@ -158,7 +149,26 @@ namespace Sdl.Web.DD4T.Mapping
             return null;
         }
 
-        private string GetFieldXPath(IField field)
+        private object GetFieldValues(IField field, Type propertyType, bool multival)
+        {
+            switch (field.FieldType)
+            {
+                case (FieldType.Date):
+                    return GetDates(field, propertyType, multival);
+                case (FieldType.Number):
+                    return GetNumbers(field, propertyType, multival);
+                case (FieldType.MultiMediaLink):
+                    return GetMultiMediaLinks(field, propertyType, multival);
+                case (FieldType.ComponentLink):
+                    return GetMultiComponentLinks(field, propertyType, multival);
+                case (FieldType.Embedded):
+                    return GetMultiEmbedded(field, propertyType, multival);
+                default:
+                    return GetStrings(field, propertyType, multival);
+            }
+        }
+
+        private static string GetFieldXPath(IField field)
         {
             return field.XPath;
         }
@@ -173,6 +183,7 @@ namespace Sdl.Web.DD4T.Mapping
             }
             return res;
         }
+
         protected virtual Dictionary<string, string> GetEntityData(IComponentPresentation cp)
         {
             if (cp != null)
@@ -185,7 +196,7 @@ namespace Sdl.Web.DD4T.Mapping
             return new Dictionary<string, string>();
         }
 
-        private object GetDates(IField field, Type modelType, bool multival)
+        private static object GetDates(IField field, Type modelType, bool multival)
         {
             if (typeof(DateTime).IsAssignableFrom(modelType))
             {
@@ -193,15 +204,13 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return field.DateTimeValues;
                 }
-                else
-                {
-                    return field.DateTimeValues[0];
-                }
+
+                return field.DateTimeValues[0];
             }
             return null;
         }
 
-        private object GetNumbers(IField field, Type modelType, bool multival)
+        private static object GetNumbers(IField field, Type modelType, bool multival)
         {
             if (typeof(Double).IsAssignableFrom(modelType))
             {
@@ -209,15 +218,13 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return field.NumericValues;
                 }
-                else
-                {
-                    return field.NumericValues[0];
-                }
+
+                return field.NumericValues[0];
             }
             return null;
         }
 
-        private object GetMultiMediaLinks(IField field, Type modelType, bool multival)
+        private static object GetMultiMediaLinks(IField field, Type modelType, bool multival)
         {
             if (typeof(Image).IsAssignableFrom(modelType))
             {
@@ -225,10 +232,8 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return GetImages(field.LinkedComponentValues);
                 }
-                else
-                {
-                    return GetImages(field.LinkedComponentValues)[0];
-                }
+
+                return GetImages(field.LinkedComponentValues)[0];
             }
             return null;
         }
@@ -238,7 +243,7 @@ namespace Sdl.Web.DD4T.Mapping
         {
             //TODO is reflection the only way to do this?
             MethodInfo method = GetType().GetMethod("GetCompLink" + (multival ? "s" : ""), BindingFlags.NonPublic | BindingFlags.Instance);
-            method = method.MakeGenericMethod(new Type[] { linkedItemType });
+            method = method.MakeGenericMethod(new[] { linkedItemType });
             return method.Invoke(this,new object[]{field.LinkedComponentValues,linkedItemType});
         }
 
@@ -252,10 +257,8 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return links;
                 }
-                else
-                {
-                    return links.Count > 0 ? links[0] : null;
-                }
+
+                return links.Count > 0 ? links[0] : null;
             }
             if (propertyType == typeof(Paragraph))
             {
@@ -264,15 +267,13 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return paras;
                 }
-                else
-                {
-                    return paras.Count > 0 ? paras[0] : null;
-                }
+
+                return paras.Count > 0 ? paras[0] : null;
             }
             return null;
         }
 
-        private object GetStrings(IField field, Type modelType, bool multival)
+        private static object GetStrings(IField field, Type modelType, bool multival)
         {
             if (typeof(String).IsAssignableFrom(modelType))
             {
@@ -280,43 +281,43 @@ namespace Sdl.Web.DD4T.Mapping
                 {
                     return field.Values;
                 }
-                else
-                {
-                    return field.Value;
-                }
+
+                return field.Value;
             }
             return null;
         }
 
-        private List<Image> GetImages(IList<IComponent> components)
+        private static List<Image> GetImages(IEnumerable<IComponent> components)
         {
             return components.Select(c => new Image { Url = c.Multimedia.Url, FileSize = c.Multimedia.Size }).ToList();
         }
 
-        private List<T> GetCompLinks<T>(IList<IComponent> components, Type linkedItemType)
+        private List<T> GetCompLinks<T>(IEnumerable<IComponent> components, Type linkedItemType)
         {
             List<T> list = new List<T>();
             foreach (var comp in components)
             {
-                list.Add((T)this.Create(comp, linkedItemType));
+                list.Add((T)Create(comp, linkedItemType));
             }
             return list;
         }
-        private T GetCompLink<T>(IList<IComponent> components, Type linkedItemType)
-        {
 
+        private T GetCompLink<T>(IEnumerable<IComponent> components, Type linkedItemType)
+        {
             return GetCompLinks<T>(components,linkedItemType)[0];
         }
 
-        private List<Link> GetLinks(IList<IFieldSet> list)
+        private List<Link> GetLinks(IEnumerable<IFieldSet> list)
         {
             var result = new List<Link>();
             foreach (IFieldSet fs in list)
             {
-                var link = new Link();
-                link.AlternateText = fs.ContainsKey("alternateText") ? fs["alternateText"].Value : null;
-                link.LinkText = fs.ContainsKey("linkText") ? fs["linkText"].Value : null;
-                link.Url = fs.ContainsKey("externalLink") ? fs["externalLink"].Value : (fs.ContainsKey("internalLink") ? LinkFactory.ResolveExtensionlessLink(fs["internalLink"].LinkedComponentValues[0].Id) : null);
+                var link = new Link
+                    {
+                        AlternateText = fs.ContainsKey("alternateText") ? fs["alternateText"].Value : null,
+                        LinkText = fs.ContainsKey("linkText") ? fs["linkText"].Value : null,
+                        Url = fs.ContainsKey("externalLink") ? fs["externalLink"].Value : (fs.ContainsKey("internalLink") ? LinkFactory.ResolveExtensionlessLink(fs["internalLink"].LinkedComponentValues[0].Id) : null)
+                    };
                 if (!String.IsNullOrEmpty(link.Url))
                 {
                     result.Add(link);
@@ -325,17 +326,19 @@ namespace Sdl.Web.DD4T.Mapping
             return result;
         }
 
-        private List<Paragraph> GetParagraphs(IList<IFieldSet> list)
+        private static List<Paragraph> GetParagraphs(IEnumerable<IFieldSet> list)
         {
             var result = new List<Paragraph>();
             foreach (IFieldSet fs in list)
             {
-                var para = new Paragraph();
-                para.Subheading = fs.ContainsKey("subheading") ? fs["subheading"].Value : null;
-                para.Content = fs.ContainsKey("content") ? fs["content"].Value : null;
-                //TODO for now we assume its an image - needs generic treatment
-                para.Media = fs.ContainsKey("media") ? GetImages(new List<IComponent> { fs["media"].LinkedComponentValues[0] })[0] : null;
-                para.Caption = fs.ContainsKey("caption") ? fs["caption"].Value : null;
+                var para = new Paragraph
+                    {
+                        Subheading = fs.ContainsKey("subheading") ? fs["subheading"].Value : null,
+                        Content = fs.ContainsKey("content") ? fs["content"].Value : null,
+                        // TODO for now we assume its an image - needs generic treatment
+                        Media = fs.ContainsKey("media") ? GetImages(new List<IComponent> { fs["media"].LinkedComponentValues[0] })[0] : null,
+                        Caption = fs.ContainsKey("caption") ? fs["caption"].Value : null
+                    };
                 result.Add(para);
             }
             return result;
