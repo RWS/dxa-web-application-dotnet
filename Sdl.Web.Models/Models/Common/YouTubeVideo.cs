431e27131185c4cf8cdb1f536d2345b88472a5a7
Bart Koopman <bkoopman@sdl.com>
2014-06-05 16:26:32 +0200

    added youtubevideo as mediaitem

---
+++ Sdl.Web.Models/Models/Common/YouTubeVideo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sdl.Web.Mvc.Models
+{
+    public class YouTubeVideo : MediaItem
+    {
+        public string Url { get; set; }
+        public string YouTubeId { get; set; }
+        public int FileSize { get; set; }
+    }
+}
