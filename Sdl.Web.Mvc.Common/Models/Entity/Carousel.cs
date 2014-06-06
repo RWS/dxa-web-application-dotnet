69535b52df2c986e2ea9ab2765e83fc3ac0a3934
Jan Horsman <jhorsman@sdl.com>
2014-06-06 16:43:24 +0200

    renamed Sdl.Web.Mvc.Models.Carousel to ItemList

--- Sdl.Web.Mvc.Common/Models/Entity/Carousel.cs
+++ Sdl.Web.Mvc.Common/Models/Entity/Carousel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Sdl.Web.Mvc.Models
 {
     [SemanticEntity(Vocab = "http://schema.org", EntityName = "ItemList", Prefix = "s", Public = true)]
-    public class Carousel : Entity
+    public class ItemList : Entity
     {
         [SemanticProperty("s:itemListElement")]
         public List<Teaser> ItemListElements { get; set; }
