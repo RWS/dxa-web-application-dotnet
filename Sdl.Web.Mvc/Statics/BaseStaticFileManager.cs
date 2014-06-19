d261efd434ae9b532119f2b651ed8ff37ab70726
Will Price <willpricework@gmail.com>
2014-06-19 18:20:09 +0200

    Basic version Language selector

--- Sdl.Web.Mvc/Statics/BaseStaticFileManager.cs
+++ Sdl.Web.Mvc/Statics/BaseStaticFileManager.cs
@@ -30,17 +30,18 @@ namespace Sdl.Web.Mvc
                     {
                         try
                         {
-                            //The HTML Design version is published in /version.json
-                            var versionUrl = String.Format("{0}/version.json", loc.Path == "" || loc.Path.StartsWith("/") ? loc.Path : "/" + loc.Path);
-                            var versionJson = Serialize(versionUrl,true);
-                            if (versionJson != null)
-                            {       
-                                version = Json.Decode(versionJson).version;
-                            }
                             //The other config etc files are bootstrapped from /system/_all.json
                             var url = String.Format("{0}/{1}/_all.json", loc.Path == "" || loc.Path.StartsWith("/") ? loc.Path : "/" + loc.Path, Configuration.SystemFolder);
                             SerializeFile(url, 2);
                             folders.Add(localizationRoot);
+
+                            //The HTML Design version is published in /version.json
+                            var versionUrl = String.Format("{0}/version.json", loc.Path == "" || loc.Path.StartsWith("/") ? loc.Path : "/" + loc.Path);
+                            var versionJson = Serialize(versionUrl, true);
+                            if (versionJson != null)
+                            {
+                                version = Json.Decode(versionJson).version;
+                            }
                         }
                         catch (Exception ex)
                         {
