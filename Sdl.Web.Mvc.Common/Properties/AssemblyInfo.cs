1de37f275d063b3536166c4d75b3e4fa828c5816
Will Price <willpricework@gmail.com>
2014-06-27 16:36:32 +0200

    Fixed issues with SiteMap and updated assembly info

--- Sdl.Web.Mvc.Common/Properties/AssemblyInfo.cs
+++ Sdl.Web.Mvc.Common/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Web.Models")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SDL Web Reference Implementation Common Assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("Sdl.Web.Models")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyCompany("SDL Tridion B.V.")]
+[assembly: AssemblyProduct("SDL Web Reference Implementation")]
+[assembly: AssemblyCopyright("Copyright ©  2014 SDL Tridion Development Lab B.V.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("7.1.0.0")]
+[assembly: AssemblyFileVersion("7.1.0.0")]
