6f05f3d18d5079f6cf2d00c9becc551981ff1596
Bart Koopman <bkoopman@sdl.com>
2014-07-14 15:00:46 +0200

    added before build task to allow adding version on build

--- Site/Properties/AssemblyInfo.cs
+++ Site/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SDL Tridion B.V.")]
 [assembly: AssemblyProduct("SDL Web Reference Implementation")]
-[assembly: AssemblyCopyright("Copyright ©  2014 SDL Tridion Development Lab B.V.")]
+[assembly: AssemblyCopyright("Copyright © 2014 SDL Tridion Development Lab B.V.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.1.0.*")]
+[assembly: AssemblyVersion("7.1.*")]
 //[assembly: AssemblyFileVersion("7.1.0.0")]
