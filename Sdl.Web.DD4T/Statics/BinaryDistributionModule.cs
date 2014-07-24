239d6b96aca2c4da259c83ec4b0e58eaafb8050a
Bart Koopman <bkoopman@sdl.com>
2014-07-24 18:27:23 +0200

    TSI-292 small cleanup and looking for solutions

--- Sdl.Web.DD4T/Statics/BinaryDistributionModule.cs
+++ Sdl.Web.DD4T/Statics/BinaryDistributionModule.cs
@@ -1,16 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web;
-using System.Configuration;
-using DD4T.Utils;
-using Sdl.Web.Mvc;
 using Sdl.Web.Common;
 
 namespace Sdl.Web.DD4T
 {
-
 	/// <summary>
 	/// HttpModule intercepting a request to a static resource, caches the resource to the file-system from the Broker DB
 	/// </summary>
@@ -21,9 +17,10 @@ namespace Sdl.Web.DD4T
 		/// Initialize this module. Attach the worker method to the BeginRequest event.
 		/// </summary>
 		/// <param name="application">Current HttpApplication</param>
-		public void Init(HttpApplication application) {
-            application.PreRequestHandlerExecute += new EventHandler(DistributionModule_OnPreRequestHandlerExecute);
-			application.BeginRequest += new EventHandler(DistributionModule_OnBeginRequest);
+		public void Init(HttpApplication application) 
+        {
+            application.PreRequestHandlerExecute += DistributionModule_OnPreRequestHandlerExecute;
+			application.BeginRequest += DistributionModule_OnBeginRequest;
 		}
 
 		/// <summary>
@@ -40,10 +37,10 @@ namespace Sdl.Web.DD4T
             HttpResponse response = context.Response;
             string urlPath = request.Url.AbsolutePath;
             urlPath = urlPath.StartsWith("/" + Configuration.StaticsFolder) ? urlPath.Substring(Configuration.StaticsFolder.Length + 1) : urlPath;
-            if (! IsBinaryUrl.IsMatch(urlPath))
+            if (!IsBinaryUrl.IsMatch(urlPath))
             {
-                Log.Debug("url {0} does not match binary url pattern, ignoring it", "");
-                Log.Trace(start, "binary-ignored", response.StatusCode.ToString());
+                Log.Debug("Url {0} does not match binary url pattern, ignoring it.", urlPath);
+                Log.Trace(start, "binary-ignored", response.StatusCode.ToString(CultureInfo.InvariantCulture));
                 return;
             }
             
@@ -53,7 +50,7 @@ namespace Sdl.Web.DD4T
                 response.StatusCode = 404;
                 response.SuppressContent = true;
                 application.CompleteRequest();
-                Log.Trace(start, "binary-not-found", response.StatusCode.ToString());
+                Log.Trace(start, "binary-not-found", response.StatusCode.ToString(CultureInfo.InvariantCulture));
                 return;
             }
             // if we got here, the file was successfully created on file-system
@@ -65,11 +62,11 @@ namespace Sdl.Web.DD4T
 
             if (fileLastModified.Subtract(ifModifiedSince).TotalSeconds < 1)
             {
-                Log.Debug("Sending 304 Not Modified");
+                Log.Debug("Sending 304 Not Modified.");
                 response.StatusCode = 304;
                 response.SuppressContent = true;
                 application.CompleteRequest();
-                Log.Trace(start, "binary-not-modified", response.StatusCode.ToString());
+                Log.Trace(start, "binary-not-modified", response.StatusCode.ToString(CultureInfo.InvariantCulture));
                 return;
             }
 
@@ -77,31 +74,26 @@ namespace Sdl.Web.DD4T
             // To make sure the right file is sent, we will transmit the file directly within the first second of the creation
             if (fileLastModified.AddSeconds(1).CompareTo(DateTime.Now) > 0) 
             {
-                Log.Debug("file was created less than 1 second ago, transmitting content directly");
+                Log.Debug("File was created less than 1 second ago, transmitting content directly.");
                 response.Clear();
                 response.TransmitFile(request.PhysicalPath);
-                Log.Trace(start, "binary-direct", response.StatusCode.ToString());
+                Log.Trace(start, "binary-direct", response.StatusCode.ToString(CultureInfo.InvariantCulture));
                 return;
             }
 
-            Log.Trace(start, "binary-processed", response.StatusCode.ToString());
+            Log.Trace(start, "binary-processed", response.StatusCode.ToString(CultureInfo.InvariantCulture));
         }
-
 
         public static void DistributionModule_OnBeginRequest(Object source, EventArgs e)
         {
-            DateTime timer = DateTime.Now;
-            
+            DateTime timer = DateTime.Now;            
             HttpContext context = HttpContext.Current;
             HttpRequest request = context.Request;
-            HttpResponse response = context.Response;
-
             string urlPath = request.Url.AbsolutePath;
-            Log.Debug(">>DistributionModule_OnBeginRequest ({0})", urlPath);
-            
+            Log.Debug(">>DistributionModule_OnBeginRequest ({0})", urlPath);            
             if (!IsBinaryUrl.IsMatch(urlPath))
             {
-                Log.Debug("url {0} does not match binary url pattern, ignoring it", urlPath);
+                Log.Debug("Url {0} does not match binary url pattern, ignoring it.", urlPath);
                 Log.Debug("<<DistributionModule_OnBeginRequest ({0})", urlPath);
                 Log.Trace(timer, "binary-skip", "");
                 return;
@@ -109,27 +101,28 @@ namespace Sdl.Web.DD4T
 
             string realPath = request.PhysicalApplicationPath + Configuration.StaticsFolder + request.Path.Replace("/", "\\"); // request.PhysicalPath;
             context.RewritePath("/" + Configuration.StaticsFolder + request.Path);
-
             if (!File.Exists(realPath))
             {
-                Log.Debug("Dir path: " + realPath.Substring(0, realPath.LastIndexOf("\\")));
+                Log.Debug("Dir path: " + realPath.Substring(0, realPath.LastIndexOf("\\", StringComparison.Ordinal)));
                 try
                 {
-                    string dir = realPath.Substring(0, realPath.LastIndexOf("\\"));
-
-                    if(!Directory.Exists(dir))
-                        Directory.CreateDirectory(dir);
-                    using (FileStream file = File.Create(realPath))
+                    string dir = realPath.Substring(0, realPath.LastIndexOf("\\", StringComparison.Ordinal));
+                    if (!Directory.Exists(dir))
                     {
-                        StreamWriter sw = new StreamWriter(file);
-                        sw.Write("");
+                        Directory.CreateDirectory(dir);
+                    }
+
+                    using (FileStream file = File.Create(realPath))
+                    using (StreamWriter sw = new StreamWriter(file))
+                    {
+                        sw.Write(String.Empty);
                         sw.Close();
                         file.Close();
                     }
                 }
-                catch (Exception exception)
+                catch (Exception ex)
                 {
-                    Log.Warn("IIS empty file could not be created." + exception.Message);
+                    Log.Warn("IIS empty file could not be created." + ex.Message);
                 }
             }
 
@@ -141,32 +134,27 @@ namespace Sdl.Web.DD4T
 		/// </summary>
 		public void Dispose() { }
 
-
         #endregion
 
         #region private
-        private IBinaryFileManager _binaryFileManager = null;
+        private IBinaryFileManager _binaryFileManager;
         public virtual IBinaryFileManager BinaryFileManager 
         {
             get
             {
-                if (_binaryFileManager == null)
-                    _binaryFileManager = new BinaryFileManager();
-                return _binaryFileManager;
+                return _binaryFileManager ?? (_binaryFileManager = new BinaryFileManager());
             }
             set
             {
                 _binaryFileManager = value;
             }
         }
-        private static Regex _isBinaryUrl = null;
+        private static Regex _isBinaryUrl;
         private static Regex IsBinaryUrl
         {
             get
             {
-                if (_isBinaryUrl == null)
-                    _isBinaryUrl = new Regex(Configuration.MediaUrlRegex);
-                return _isBinaryUrl;
+                return _isBinaryUrl ?? (_isBinaryUrl = new Regex(Configuration.MediaUrlRegex));
             }
         }
 
