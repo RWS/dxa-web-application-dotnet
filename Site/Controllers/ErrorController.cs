7c330b0823acabf9cd428f2be06c421170b0ab69
tstrypko <tstrypko@sdl.com>
2014-08-07 19:34:02 +0300

    * Implement TSI-245 -- simple, controller-based 500 and 404 error pages as per [http://www.digitallycreated.net/Blog/57/getting-the-correct-http-status-codes-out-of-asp.net-custom-error-pages]

---
+++ Site/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace Sdl.Web
+{
+    public partial class ErrorController : Controller
+    {
+        [PreventDirectAccess]
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+
+        [PreventDirectAccess]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        [PreventDirectAccess]
+        public ActionResult OtherHttpStatusCode(int httpStatusCode)
+        {
+            return View("GenericHttpError", httpStatusCode);
+        }
+
+        private class PreventDirectAccessAttribute : FilterAttribute, IAuthorizationFilter
+        {
+            public void OnAuthorization(AuthorizationContext filterContext)
+            {
+                object value = filterContext.RouteData.Values["fromAppErrorEvent"];
+                if (!(value is bool && (bool)value))
+                    filterContext.Result = new ViewResult { ViewName = "Error404" };
+            }
+        }
+    }
+}
