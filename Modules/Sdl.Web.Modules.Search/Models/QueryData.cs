8a9d4465fb40831883256bae19b90783f75d6c26
Will Price <wprice@sdl.com>
2014-09-19 14:04:55 +0200

    Added first draft of Search Module (using SI4T+Solr)

---
+++ Modules/Sdl.Web.Modules.Search/Models/QueryData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sdl.Web.Modules.Search
+{
+    public class QueryData
+    {
+        public string QueryText { get; set; }
+    }
+}
---
+++ Modules/Sdl.Web.Modules.Search/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Sdl.Web.Common.Interfaces;
+using Sdl.Web.Common.Models;
+using Sdl.Web.Mvc.Controllers;
+
+namespace Sdl.Web.Modules.Search
+{
+    public class SearchController : BaseController
+    {
+        public virtual ISearchProvider SearchProvider { get; set; }
+        public SearchController(IContentProvider contentProvider, IRenderer renderer, ISearchProvider searchProvider)
+        {
+            ContentProvider = contentProvider;
+            SearchProvider = searchProvider;
+            Renderer = renderer;
+        }
+
+        override protected object ProcessModel(object sourceModel, System.Type type)
+        {
+            var model = base.ProcessModel(sourceModel, type);
+            var results = model as SearchResults<Teaser>;
+            if (results != null) 
+            {
+                results.Query = new QueryData();
+                results.Query.QueryText = Request.Params["q"];
+                model = SearchProvider.ExecuteQuery(results);
+            }
+            return model;
+        }
+    }
+}
