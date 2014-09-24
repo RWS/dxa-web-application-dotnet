ef1dcb78479b726901e9a5494c7ceaec0e3a8121
Will Price <wprice@sdl.com>
2014-09-24 12:17:43 +0200

    Added VersionInfo.cs to Search Module and post build to copy distributable dlls

--- Modules/Sdl.Web.Modules.Search/Properties/AssemblyInfo.cs
+++ Modules/Sdl.Web.Modules.Search/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Web.Modules.Search")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SDL Tridion Reference Implementation Search Module Assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("Sdl.Web.Modules.Search")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyCompany("SDL Tridion B.V.")]
+[assembly: AssemblyProduct("SDL Tridion Reference Implementation")]
+[assembly: AssemblyCopyright("Copyright © 2014 SDL Tridion Development Lab B.V.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cdc20f51-53d2-406b-a5cd-0ef570a5d149")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// version information set in VersionInfo.cs so it can be updated by build server 
