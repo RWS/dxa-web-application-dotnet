0813705aa6e9510ebf80411573e1c7831f0df707
Bart Koopman <bkoopman@sdl.com>
2014-10-13 11:56:33 +0200

    TSI-179 added 1x1px transparent image to teaser overlay with image, if there is no image available to fix carousel problems.

--- Sdl.Web.Mvc/Html/HtmlHelperExtensions.cs
+++ Sdl.Web.Mvc/Html/HtmlHelperExtensions.cs
@@ -166,7 +166,7 @@ namespace Sdl.Web.Mvc.Html
                 builder.Attributes.Add("width", imgWidth);
             }
             builder.Attributes.Add("alt", image.AlternateText);
-            builder.Attributes.Add("data-aspect", (Math.Truncate(aspect * 100) / 100).ToString());
+            builder.Attributes.Add("data-aspect", (Math.Truncate(aspect * 100) / 100).ToString(CultureInfo.InvariantCulture));
             if (!String.IsNullOrEmpty(cssClass))
             {
                 builder.Attributes.Add("class", cssClass);
