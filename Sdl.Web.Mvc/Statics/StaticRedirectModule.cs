8962a5f995e6c211a94a7092bb9450039e1c0f26
Will Price <wprice@sdl.com>
2014-10-15 11:00:20 +0200

    Prevent direct access to BinaryData folder in StaticRedirectModule - required to prevent cross site binary access for Multi-tenant scenario

--- Sdl.Web.Mvc/Statics/StaticRedirectModule.cs
+++ Sdl.Web.Mvc/Statics/StaticRedirectModule.cs
@@ -7,9 +7,17 @@ using Sdl.Web.Mvc.Configuration;
 namespace Sdl.Web.Mvc.Statics
 {
     /// <summary>
-    /// Module to redirect requests for version of static files which no longer exist on disk to the root assets folder
-    /// When a request for a static file comes into IIS, it first checks if it exists on disk, and if so, serves it normally
-    /// If it does not exist, it enters the ASP.NET pipeline, where this module will pick it up and redirect it
+    /// This module has 2 tasks:
+    /// 1. Remove the version from the URL of HTML design assets - links to design assets are written out 
+    ///    with a version number in the URL (eg /system/v0.60/assets/css/main.css). This version
+    ///    does not correspond to any real path, but is purely a mechanism to allow us to avoid
+    ///    browser caching issues when the css is updated (we simply update the version number)
+    /// 2. Prevent direct requests for binary content that has been serialized to disk - we serialize
+    ///    binaries to the /BinaryData folder of the website, and binary requests are rewritten to this
+    ///    path, however in the situation where mutiple sites are hosted by the same web application we 
+    ///    dont want to allow binaries from one site to be shown on the other. Normally this would be possible
+    ///    if you know the publication id and the URL of the image using the a URL like:
+    ///    /BinaryData/12/media/image-from-another-site.jpg
     /// </summary>
     public class StaticRedirectModule : IHttpModule
     {
@@ -24,6 +32,13 @@ namespace Sdl.Web.Mvc.Statics
             HttpContext context = application.Context;
             var url = context.Request.Url.AbsolutePath;
             Log.Debug("StaticRedirectModule_BeginRequest: " + url);
+            if (url.StartsWith("/" + SiteConfiguration.StaticsFolder + "/"))
+            {
+                //Block direct access to BinaryData folder
+                context.Response.StatusCode = 404;
+                context.Response.End();
+                return;
+            }
             var versionLessUrl = SiteConfiguration.RemoveVersionFromPath(url);
             if (url != versionLessUrl)
             {
