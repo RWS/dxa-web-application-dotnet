7b327d334296744452501c7d6a76e992b8e94200
Will Price <wprice@sdl.com>
2014-10-22 12:02:51 +0200

    Updated DD4T refs rebuild with latest ContentModel changes

---
+++ Modules/Tri.Example.Product/MyProductDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tri.Example.Products
+{
+    public class MyProductDataProvider : IProductDataProvider
+    {
+        public void FetchProductData(Product model)
+        {
+            model.Price = 99.99;
+            model.Stock = 10;
+        }
+    }
+}
