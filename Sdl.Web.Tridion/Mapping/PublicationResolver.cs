1d87567314375c0ce564aa36543f599cd468b459
Bart Koopman <bkoopman@sdl.com>
2015-07-20 17:04:08 +0200

    TSI-117 caught WebRequestContext.Localization being null on some requests which only seemed to be in the way when debugging (error was never logged and page rendered correctly), so returning 0 as Publication ID now (hope that is correct).

--- Sdl.Web.Tridion/Mapping/PublicationResolver.cs
+++ Sdl.Web.Tridion/Mapping/PublicationResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DD4T.ContentModel.Contracts.Resolvers;
+using Sdl.Web.Common.Logging;
 using Sdl.Web.Mvc.Configuration;
 
 namespace Sdl.Web.Tridion.Mapping
@@ -8,7 +9,18 @@ namespace Sdl.Web.Tridion.Mapping
     {
         public int ResolvePublicationId()
         {
-            return Int32.Parse(WebRequestContext.Localization.LocalizationId);
+            try
+            {
+                return Int32.Parse(WebRequestContext.Localization.LocalizationId);
+            }
+            catch (NullReferenceException e)
+            {
+                // catching this error so it is not in the way when debugging
+                Log.Debug("WebRequestContext.Localization is null{0}", e.StackTrace);
+
+                // TODO: should it be allowed that WebRequestContext.Localization is null, and can we safely return 0 in that case?
+                return 0;
+            }
         }
     }
 }
