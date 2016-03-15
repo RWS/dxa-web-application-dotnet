8ba4e5aabdadc0cf364ff65d2688091f8f4ca0b3
Paul Adams <padams@sdl.com>
2016-03-15 13:53:46 +0000

    TSI-1389 : cid proxy support added

--- Sdl.Web.Mvc/Html/ContextualMediaHelper.cs
+++ Sdl.Web.Mvc/Html/ContextualMediaHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Web.Configuration;
 using Sdl.Web.Common;
 using Sdl.Web.Common.Configuration;
@@ -12,18 +14,21 @@ namespace Sdl.Web.Mvc.Html
     /// </summary>
     public class ContextualMediaHelper : BaseMediaHelper
     {
-        private string _cidBaseUrl;
+        private readonly string _cidBaseUrl;
 
         public ContextualMediaHelper()
         {
             ImageResizeUrlFormat = "{0}/scale/{1}x{2}/{3}{4}";
-            _cidBaseUrl = WebConfigurationManager.AppSettings["cid-service-uri"] ?? "/cid";
-            if (_cidBaseUrl == string.Empty)
+            // the exclusion of the app setting does mean the proxy will operate on all requests and we don't want that
+            // so we must force the use of this setting by triggering an exception if its empty
+            string pattern = WebConfigurationManager.AppSettings["cid-service-proxy-pattern"] ?? string.Empty;
+            _cidBaseUrl = pattern.Replace("*", "").Replace("?", "").TrimEnd('/');
+            if (string.IsNullOrEmpty(_cidBaseUrl))
             {
-                throw new DxaException("cid-service-uri cannot be empty when ContextualMediaHelper is enabled.");
-            }
+                throw new DxaException("cid-service-proxy-pattern cannot be empty when ContextualMediaHelper is enabled.");
+            }          
         }
-
+       
         public override string GetResponsiveImageUrl(string url, double aspect, string widthFactor, int containerSize = 0)
         {
             int width = GetResponsiveWidth(widthFactor, containerSize);
@@ -44,7 +49,8 @@ namespace Sdl.Web.Mvc.Html
             //Build the URL
             url = SiteConfiguration.MakeFullUrl(url, WebRequestContext.Localization);
             string prefix = url.StartsWith("https") ? "https/" : string.Empty;
-            url = url.Substring(url.IndexOf("://", StringComparison.Ordinal) + 3);
+            // should encode the url incase it contains special chars in a query string or something
+            url = WebUtility.UrlEncode(url.Substring(url.IndexOf("://", StringComparison.Ordinal) + 3));
             return String.Format(ImageResizeUrlFormat, _cidBaseUrl, width, height, prefix, url);
         }
     }
