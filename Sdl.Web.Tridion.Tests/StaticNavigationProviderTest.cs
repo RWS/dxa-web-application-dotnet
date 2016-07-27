8a4c5feca81b902bcb494b58835a15254b673f68
Rick Pannekoek <rpannekoek@sdl.com>
2016-07-27 19:45:33 +0200

    TSI-1638 Split DefaultProvider into DefaultContentProvider and StaticNavigationProvider (TSI-1759)

---
+++ Sdl.Web.Tridion.Tests/StaticNavigationProviderTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sdl.Web.Common.Configuration;
+using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Tridion.Tests
+{
+    [TestClass]
+    public class StaticNavigationProviderTest : TestClass
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            DefaultInitialize(testContext);
+        }
+
+        [TestMethod]
+        public void GetNavigationModel_Success()
+        {
+            SitemapItem rootSitemapItem = SiteConfiguration.NavigationProvider.GetNavigationModel(TestFixture.ParentLocalization);
+
+            Assert.IsNotNull(rootSitemapItem, "Result of NavigationProvider.GetNavigationModel");
+            Assert.AreEqual("Home", rootSitemapItem.Title, "Root SitemapItem Title");
+            Assert.AreEqual("StructureGroup", rootSitemapItem.Type, "Root SitemapItem Type");
+            Assert.AreEqual(TestFixture.ParentLocalization.Path + "/", rootSitemapItem.Url, "Root SitemapItem Url");
+        }
+
+    }
+}
