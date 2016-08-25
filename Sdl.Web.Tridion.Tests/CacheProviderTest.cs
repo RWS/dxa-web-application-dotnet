a070121631f55178499df48a7948c536b17038da
Rick Pannekoek <rpannekoek@sdl.com>
2016-08-25 09:48:17 +0200

    TSI-1638 Added CacheProviderTest.TryGet_WrongType_Exception

--- Sdl.Web.Tridion.Tests/CacheProviderTest.cs
+++ Sdl.Web.Tridion.Tests/CacheProviderTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sdl.Web.Common;
 using Sdl.Web.Common.Interfaces;
 
 namespace Sdl.Web.Tridion.Tests
@@ -7,6 +8,9 @@ namespace Sdl.Web.Tridion.Tests
     [TestClass]
     public class CacheProviderTest : TestClass
     {
+        private const string TestKey1 = "TestKey1";
+        private const string TestKey2 = "TestKey2";
+
         private static readonly ICacheProvider _testCacheProvider = new DefaultCacheProvider();
 
         [ClassInitialize]
@@ -20,28 +24,26 @@ namespace Sdl.Web.Tridion.Tests
         {
             const int testValue1 = 666;
             const int testValue2 = 999;
-            const string testKey1 = "TestKey1";
-            const string testKey2 = "TestKey2";
             const string testRegion1 = "TryGetStore_IntValue_Success_1";
             const string testRegion2 = "TryGetStore_IntValue_Success_2";
 
             int cachedValue;
-            Assert.IsFalse(_testCacheProvider.TryGet(testKey1, testRegion1, out cachedValue));
-            Assert.IsFalse(_testCacheProvider.TryGet(testKey2, testRegion1, out cachedValue));
-            Assert.IsFalse(_testCacheProvider.TryGet(testKey1, testRegion2, out cachedValue));
+            Assert.IsFalse(_testCacheProvider.TryGet(TestKey1, testRegion1, out cachedValue));
+            Assert.IsFalse(_testCacheProvider.TryGet(TestKey2, testRegion1, out cachedValue));
+            Assert.IsFalse(_testCacheProvider.TryGet(TestKey1, testRegion2, out cachedValue));
 
-            _testCacheProvider.Store(testKey1, testRegion1, testValue1);
-            _testCacheProvider.Store(testKey2, testRegion1, testValue2);
+            _testCacheProvider.Store(TestKey1, testRegion1, testValue1);
+            _testCacheProvider.Store(TestKey2, testRegion1, testValue2);
 
-            Assert.IsTrue(_testCacheProvider.TryGet(testKey1, testRegion1, out cachedValue));
+            Assert.IsTrue(_testCacheProvider.TryGet(TestKey1, testRegion1, out cachedValue));
             Assert.AreEqual(testValue1, cachedValue);
-            Assert.IsTrue(_testCacheProvider.TryGet(testKey2, testRegion1, out cachedValue));
+            Assert.IsTrue(_testCacheProvider.TryGet(TestKey2, testRegion1, out cachedValue));
             Assert.AreEqual(testValue2, cachedValue);
-            Assert.IsFalse(_testCacheProvider.TryGet(testKey1, testRegion2, out cachedValue));
+            Assert.IsFalse(_testCacheProvider.TryGet(TestKey1, testRegion2, out cachedValue));
 
-            _testCacheProvider.Store(testKey1, testRegion1, testValue2);
+            _testCacheProvider.Store(TestKey1, testRegion1, testValue2);
 
-            Assert.IsTrue(_testCacheProvider.TryGet(testKey1, testRegion1, out cachedValue));
+            Assert.IsTrue(_testCacheProvider.TryGet(TestKey1, testRegion1, out cachedValue));
             Assert.AreEqual(testValue2, cachedValue);
         }
 
@@ -49,14 +51,24 @@ namespace Sdl.Web.Tridion.Tests
         public void GetOrAdd_IntValue_Success()
         {
             const int testValue = 666;
-            const string testKey1 = "TestKey1";
-            const string testRegion1 = "GetOrAdd_IntValue_Success";
+            const string testRegion = "GetOrAdd_IntValue_Success";
 
-            int cachedValue = _testCacheProvider.GetOrAdd(testKey1, testRegion1, () => { Thread.Sleep(1000); return testValue; });
+            int cachedValue = _testCacheProvider.GetOrAdd(TestKey1, testRegion, () => { Thread.Sleep(100); return testValue; });
             Assert.AreEqual(testValue, cachedValue);
 
-            cachedValue = _testCacheProvider.GetOrAdd(testKey1, testRegion1, () => { Assert.Fail("Add function called second time."); return 0; });
+            cachedValue = _testCacheProvider.GetOrAdd(TestKey1, testRegion, () => { Assert.Fail("Add function called second time."); return 0; });
             Assert.AreEqual(testValue, cachedValue);
+        }
+
+        [TestMethod]
+        public void TryGet_WrongType_Exception()
+        {
+            const string testRegion = "TryGet_WrongType_Exception";
+
+            _testCacheProvider.Store(TestKey1, testRegion, 666);
+
+            string cachedValue;
+            AssertThrowsException<DxaException>(() => { _testCacheProvider.TryGet(TestKey1, testRegion, out cachedValue); }, "TryGet");
         }
     }
 }
--- Sdl.Web.Tridion.Tests/TestClass.cs
+++ Sdl.Web.Tridion.Tests/TestClass.cs
@@ -26,5 +26,20 @@ namespace Sdl.Web.Tridion.Tests
             Console.WriteLine("---- JSON Representation of {0} ----", objectToSerialize.GetType().FullName);
             Console.WriteLine(json);
         }
+
+        protected void AssertThrowsException<TException>(Action action, string actionName = null)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail("Action {0} did not throw an exception. Expected exception {1}.", actionName, typeof(TException).Name);
+            }
+            catch (TException ex)
+            {
+                Console.WriteLine("Expected exception was thrown by action {0}:", actionName);
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
