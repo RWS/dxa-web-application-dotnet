ad1c451ceac6685b9614a9ab8e5f09cf7b555c0f
Paul Adams <majiccode@gmail.com>
2017-02-16 10:51:02 +0000

    Removed redundant using

--- Sdl.Web.Common/Models/ModelTypeRegistry.cs
+++ Sdl.Web.Common/Models/ModelTypeRegistry.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Web.Compilation;
 using Sdl.Web.Common.Logging;
 using Sdl.Web.Common.Mapping;
