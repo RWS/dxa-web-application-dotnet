4208689e81410699a22e4345926d6e6745ca57e3
Paul Adams <majiccode@gmail.com>
2017-09-04 10:44:59 +0100

    TSI-2614 : refactor

--- Sdl.Web.Tridion/Mapping/PublicationResolver.cs
+++ Sdl.Web.Tridion/Mapping/PublicationResolver.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using DD4T.ContentModel.Contracts.Resolvers;
-using Sdl.Web.Common.Configuration;
+using Sdl.Web.Mvc.Configuration;
 
 namespace Sdl.Web.Tridion.Mapping
 {
-    internal class PublicationResolver : IPublicationResolver
-    {
-        private readonly int _publicationId;
-
-        public PublicationResolver(Localization localization)
-        {
-            _publicationId = Convert.ToInt32(localization.Id);
-        }
-
+    public class PublicationResolver : IPublicationResolver
+    {       
         public int ResolvePublicationId()
         {
-            return _publicationId;
+            return Convert.ToInt32(WebRequestContext.Localization.Id);
         }
     }
 }
