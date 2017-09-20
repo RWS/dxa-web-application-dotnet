0cbc4d565b65ac460113d1029bd5d14fd6e5dacf
Paul Adams <majiccode@gmail.com>
2017-09-20 11:50:42 +0100

    TSI-2714 : implemented ICacheAgent

---
+++ DD4T/DD4T.Providers.DxaModelService/Caching/DependencyMappings.cs
@@ -0,0 +1,20 @@
+﻿using DD4T.ContentModel.Contracts.Caching;
+using DD4T.Core.Contracts.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace DD4T.Providers.DxaModelService.Caching
+{
+    public class DependencyMappings : IDependencyMapper
+    {
+        private IDictionary<Type, Type> GetMappings() => new Dictionary<Type, Type> {{typeof (ICacheAgent), typeof (DxaCacheAgent)}};
+
+        public IDictionary<Type, Type> SingleInstanceMappings => GetMappings();
+
+        public IDictionary<Type, Type> PerHttpRequestMappings => null;
+
+        public IDictionary<Type, Type> PerLifeTimeMappings => null;
+
+        public IDictionary<Type, Type> PerDependencyMappings => null;
+    }
+}
