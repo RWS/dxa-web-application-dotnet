f3a6087da520d6ef005718e6fc123faa60046fa7
Paul Adams <majiccode@gmail.com>
2017-11-02 14:19:40 +0000

    TSI-2894 : zero width now considered to use default

--- Sdl.Web.Mvc/Configuration/WebRequestContext.cs
+++ Sdl.Web.Mvc/Configuration/WebRequestContext.cs
@@ -31,14 +31,9 @@ namespace Sdl.Web.Mvc.Configuration
         /// <summary>
         /// The Tridion Context Engine
         /// </summary>
-        public static ContextEngine ContextEngine
-        {
-            get
-            {
-                return (ContextEngine)GetFromContextStore("ContextEngine") ?? (ContextEngine)AddToContextStore("ContextEngine", new ContextEngine());
-            }
-        }
-        
+        public static ContextEngine ContextEngine 
+            => (ContextEngine)GetFromContextStore("ContextEngine") ?? (ContextEngine)AddToContextStore("ContextEngine", new ContextEngine());
+
         /// <summary>
         /// The maximum width for media objects for this requests display width
         /// </summary>
@@ -61,31 +56,19 @@ namespace Sdl.Web.Mvc.Configuration
             get
             {
                 object val = GetFromContextStore("ScreenWidth");
-                return val == null ? (ScreenWidth)AddToContextStore("ScreenWidth", CalculateScreenWidth()) : (ScreenWidth)val;
+                return (ScreenWidth?) val ?? (ScreenWidth)AddToContextStore("ScreenWidth", CalculateScreenWidth());
             }
         }
 
         /// <summary>
         /// The current request URL
         /// </summary>
-        public static string RequestUrl
-        {
-            get
-            {
-                return HttpContext.Current.Request.Url.ToString();
-            }
-        }
+        public static string RequestUrl => HttpContext.Current.Request.Url.ToString();
 
         /// <summary>
         /// String array of client-supported MIME accept types
         /// </summary>
-        public static string[] AcceptTypes
-        {
-            get 
-            { 
-                return HttpContext.Current.Request.AcceptTypes;
-            }
-        }
+        public static string[] AcceptTypes => HttpContext.Current.Request.AcceptTypes;
 
         /// <summary>
         /// Current Page Model
@@ -105,13 +88,7 @@ namespace Sdl.Web.Mvc.Configuration
         /// <summary>
         /// True if the request is for localhost domain
         /// </summary>
-        public static bool IsDeveloperMode
-        {
-            get
-            {
-                return (bool?)GetFromContextStore("IsDeveloperMode") ?? (bool)AddToContextStore("IsDeveloperMode", GetIsDeveloperMode());
-            }
-        }
+        public static bool IsDeveloperMode => (bool?)GetFromContextStore("IsDeveloperMode") ?? (bool)AddToContextStore("IsDeveloperMode", GetIsDeveloperMode());
 
         private static bool GetIsDeveloperMode()
         {
@@ -132,30 +109,21 @@ namespace Sdl.Web.Mvc.Configuration
         /// <summary>
         /// True if the request is from XPM (NOTE currently always true for staging as we cannot reliably distinguish XPM requests)
         /// </summary>
-        public static bool IsPreview
-        {
-            //For now we cannot reliably detect when we are in experience manager, so we set this to be true whenever we are in staging
-            get
-            {
-                return (bool?)GetFromContextStore("IsPreview") ?? (bool)AddToContextStore("IsPreview", Localization.IsXpmEnabled);
-            }
-        }
+        public static bool IsPreview 
+            => (bool?)GetFromContextStore("IsPreview") ?? (bool)AddToContextStore("IsPreview", Localization.IsXpmEnabled);
 
         /// <summary>
         /// True if the request is an include page
         /// </summary>
-        public static bool IsInclude
-        {
-            // if request url contains "system/include" the include page is requested directly
-            get
-            {
-                return (bool?)GetFromContextStore("IsInclude") ?? (bool)AddToContextStore("IsInclude", RequestUrl.Contains("system/include/"));
-            }
-        }
+        public static bool IsInclude 
+            => (bool?)GetFromContextStore("IsInclude") ?? (bool)AddToContextStore("IsInclude", RequestUrl.Contains("system/include/"));
 
         protected static ScreenWidth CalculateScreenWidth()
         {
-            int width = IsContextCookiePresent ? ContextEngine.GetClaims<BrowserClaims>().DisplayWidth : 1024;
+            int width = IsContextCookiePresent ? ContextEngine.GetClaims<BrowserClaims>().DisplayWidth : MaxWidth;
+            // zero width is not valid and probably means the context engine was not correctly initialized so
+            // again default to 1024
+            if (width == 0) width = MaxWidth;
             if (width < SiteConfiguration.MediaHelper.SmallScreenBreakpoint)
             {
                 return ScreenWidth.ExtraSmall;
@@ -171,26 +139,16 @@ namespace Sdl.Web.Mvc.Configuration
             return ScreenWidth.Large;
         }
 
-        protected static Localization GetCurrentLocalization()
-        {
-            if (HttpContext.Current == null)
-            {
-                return null;
-            }
-            return SiteConfiguration.LocalizationResolver.ResolveLocalization(HttpContext.Current.Request.Url);
-        }
-        
-        protected static object GetFromContextStore(string key)
-        {
-            return HttpContext.Current == null ? null : HttpContext.Current.Items[key];
-        }
+        protected static Localization GetCurrentLocalization() 
+            => HttpContext.Current == null ? null : SiteConfiguration.LocalizationResolver.ResolveLocalization(HttpContext.Current.Request.Url);
+
+        protected static object GetFromContextStore(string key) 
+            => HttpContext.Current == null ? null : HttpContext.Current.Items[key];
 
         protected static object AddToContextStore(string key, object value)
         {
-            if (HttpContext.Current != null)
-            {
-                HttpContext.Current.Items[key] = value;
-            }
+            if (HttpContext.Current == null) return value;
+            HttpContext.Current.Items[key] = value;
             return value;
         }
 
@@ -199,7 +157,7 @@ namespace Sdl.Web.Mvc.Configuration
             get
             {
                 HttpContext httpContext = HttpContext.Current;
-                return (httpContext != null) && (httpContext.Request.Cookies["context"] != null);
+                return httpContext?.Request.Cookies["context"] != null;
             }
         }
     }
