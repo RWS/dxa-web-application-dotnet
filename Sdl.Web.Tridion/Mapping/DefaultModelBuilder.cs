a0ce583cd1476f447c137ed275c7c2f80d12d85c
Paul Adams <majiccode@gmail.com>
2018-04-04 13:02:01 +0100

    TSI-3129 : Improve handling of missing resources

--- Sdl.Web.Tridion/Mapping/DefaultModelBuilder.cs
+++ Sdl.Web.Tridion/Mapping/DefaultModelBuilder.cs
@@ -810,12 +810,27 @@ namespace Sdl.Web.Tridion.Mapping
         {
             if (pageModelData.MvcData?.ViewName == "IncludePage") return pageModelData.Title;
             IDictionary coreResources = localization.GetResources("core");
-            string title = "defaultPageTitle".Equals(pageModelData.Title)
-                ? coreResources["core.defaultPageTitle"].ToString()
-                : pageModelData.Title;
-            string separator = coreResources["core.pageTitleSeparator"].ToString();
-            string suffix = coreResources["core.pageTitlePostfix"].ToString();
+
+            string title = pageModelData.Title;
+            if (coreResources.Contains("core.defaultPageTitle"))
+            {
+                title = "defaultPageTitle".Equals(pageModelData.Title)
+                    ? coreResources["core.defaultPageTitle"].ToString()
+                    : pageModelData.Title;
+            }
+            string separator = string.Empty;
+            if (coreResources.Contains("core.pageTitleSeparator"))
+            {
+                separator = coreResources["core.pageTitleSeparator"].ToString();
+            }
+
+            string suffix = string.Empty;
+            if (coreResources.Contains(""))
+            {
+                coreResources["core.pageTitlePostfix"].ToString();
+            }
+
             return $"{title}{separator}{suffix}";
-        }      
+        }
     }
 }
