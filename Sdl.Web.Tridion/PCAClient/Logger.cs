eab25ef1d7910e74d11759b3b7d26cc0d87d5d59
Paul Adams <majiccode@gmail.com>
2018-08-13 11:59:37 +0100

    TSI-3231 : Added missing file

---
+++ Sdl.Web.Tridion/PCAClient/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sdl.Web.Common.Logging;
+using Sdl.Web.Core;
+
+namespace Sdl.Web.Tridion.PCAClient
+{
+    public class Logger : ILogger
+    {
+        protected string Reformat(string msg)
+        {
+            return msg.Replace("{", "{{").Replace("}", "}}");
+        }
+
+        public void Trace(string messageFormat, params object[] parameters) => Log.Trace(Reformat(messageFormat), parameters);
+        public void Debug(string messageFormat, params object[] parameters) => Log.Debug(Reformat(messageFormat), parameters);
+        public void Info(string messageFormat, params object[] parameters) => Log.Info(Reformat(messageFormat), parameters);
+        public void Warn(string messageFormat, params object[] parameters) => Log.Warn(Reformat(messageFormat), parameters);
+        public void Error(string messageFormat, params object[] parameters) => Log.Error(Reformat(messageFormat), parameters);
+        public void Error(Exception ex, string messageFormat, params object[] parameters) => Log.Error(ex, Reformat(messageFormat), parameters);
+        public void Error(Exception ex) => Log.Error(ex);
+        public bool IsTracingEnabled => Log.IsTraceEnabled;
+        public bool IsDebugEnabled => Log.IsDebugEnabled;
+    }
+}
