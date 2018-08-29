502f8025cd448e29f34a85325967f6bb8101c194
Paul Adams <padams@sdl.com>
2018-08-29 11:09:34 +0100

    TSI-3419 : Fix tests

--- Sdl.Web.Tridion.Tests/TestFixture.cs
+++ Sdl.Web.Tridion.Tests/TestFixture.cs
@@ -35,8 +35,10 @@ namespace Sdl.Web.Tridion.Tests
 
     internal class TestFixture : ILocalizationResolver
     {
-        internal const string HomePageId = "640";
-        internal const string ArticleDcpEntityId = "9712-9711";
+        //internal const string HomePageId = "640"; // dxadevweb85.ams.dev
+        internal const string HomePageId = "279";
+        //internal const string ArticleDcpEntityId = "9712-9711";
+        internal const string ArticleDcpEntityId = "462-644";
         internal const string NavigationTaxonomyTitle = "Test Taxonomy [Navigation]";
         internal const string TopLevelKeyword1Title = "Top-level Keyword 1";
         internal const string TopLevelKeyword2Title = "Top-level Keyword 2";
@@ -54,7 +56,8 @@ namespace Sdl.Web.Tridion.Tests
         internal const string TaxonomyIndexPageRelativeUrlPath = "regression/taxonomy";
         internal const string Tsi811PageRelativeUrlPath = "regression/tsi-811";
         internal const string Tsi1278PageRelativeUrlPath = "tsi-1278_trådløst.html";
-        internal const string Tsi1278StaticContentItemRelativeUrlPath = "Images/trådløst_tcm{0}-9791.jpg";
+        //internal const string Tsi1278StaticContentItemRelativeUrlPath = "Images/trådløst_tcm{0}-9791.jpg";
+        internal const string Tsi1278StaticContentItemRelativeUrlPath = "Images/trådløst_tcm{0}-510.jpg";
         internal const string Tsi1308PageRelativeUrlPath = "regression/tsi-1308";
         internal const string Tsi1757PageRelativeUrlPath = "regression/tsi-1757";
         internal const string Tsi1614PageRelativeUrlPath = "tsi-1614.html";
@@ -89,8 +92,8 @@ namespace Sdl.Web.Tridion.Tests
         };
 
         static TestFixture()
-        {           
-            /* dxadevwev85.ams.dev*/
+        {
+            /* dxadevwev85.ams.dev
             _parentLocalization = new Localization
             {
                 Id = "1065",
@@ -113,11 +116,10 @@ namespace Sdl.Web.Tridion.Tests
             {
                 Id = "1083",
                 Path = "/autotest-child-legacy"
-            };
-            
+            };*/
 
-            // http://cm.dev.dxa.sdldev.net
-            /*
+
+            /* http://cm.dev.dxa.sdldev.net */
             _parentLocalization = new Localization
             {
                 Id = "6",
@@ -141,7 +143,7 @@ namespace Sdl.Web.Tridion.Tests
                 Id = "9",
                 Path = "/autotest-child-legacy"
             };
-            */
+
             _testLocalizations = new[] { _parentLocalization, _childLocalization, _legacyParentLocalization, _legacyChildLocalization };
 
             TestRegistration.RegisterViewModels();
