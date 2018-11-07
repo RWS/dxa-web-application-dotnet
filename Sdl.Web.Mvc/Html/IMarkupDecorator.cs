c91f982cd615c3018b3bdddb1bbc9b12fffa92b9
Rick Pannekoek <rpannekoek@sdl.com>
2018-11-07 14:17:40 +0100

    Cleanup build warnings.

--- Sdl.Web.Mvc/Html/IMarkupDecorator.cs
+++ Sdl.Web.Mvc/Html/IMarkupDecorator.cs
@@ -8,7 +8,7 @@ namespace Sdl.Web.Mvc.Html
     /// </summary>
     /// <remarks>
     /// Markup Decorators can be used to post-process the HTML rendered by Entity or Region Views.
-    /// They are invoked by the <see cref="HtmlHelperExtensions.DxaEntity"/> and <see cref="HtmlHelperExtensions.DxaRegion"/> helper methods.
+    /// They are invoked by the HtmlHelperExtensions.DxaEntity and HtmlHelperExtensions.DxaRegion helper methods.
     /// They are registered in Area Registration code uusing the <see cref="BaseAreaRegistration.RegisterMarkupDecorator"/> method.
     /// </remarks>
     public interface IMarkupDecorator
