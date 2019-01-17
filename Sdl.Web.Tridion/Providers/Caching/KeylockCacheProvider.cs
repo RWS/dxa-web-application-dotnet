b948107f9c2e08b44bec041e66b13f597f7fa658
Paul Adams <padams@sdl.com>
2019-01-17 13:16:38 +0000

    CRQ-13125 : Fix up KeyLockCacheProvider

--- Sdl.Web.Tridion/Providers/Caching/KeylockCacheProvider.cs
+++ Sdl.Web.Tridion/Providers/Caching/KeylockCacheProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Sdl.Web.Common;
 using Sdl.Web.Common.Interfaces;
 using Sdl.Web.Delivery.Caching;
 
@@ -34,65 +35,59 @@ namespace Sdl.Web.Tridion.Caching
             }
         }
 
-        public bool TryGet<T>(string key, string region, out T value)
+        public bool TryGet<T>(string key, string region, out T value) => TryGetCachedValue(key, region, out value);
+
+        public T GetOrAdd<T>(string key, string region, Func<T> addFunction, IEnumerable<string> dependencies = null)
         {
-            var cachedValue = GetCachedValue<T>(key, region);
+            T cachedValue;
+            if (TryGetCachedValue(key, region, out cachedValue)) return cachedValue;
+            var hash = CalcHashKey(key, region);
+            lock (KeyLocks.GetOrAdd(hash, new object()))
+            {
+                try
+                {
+                    // Try and get from cache again in case it has been added in the meantime
+                    if (TryGetCachedValue(key, region, out cachedValue)) return cachedValue;
+
+                    // Still null, so lets run Func()
+                    cachedValue = addFunction();
+                    if (cachedValue != null)
+                    {
+                        // Note that dependencies are not used?
+                        Debug.Assert(_cilCacheProvider != null, "_cilCacheProvider != null");
+                        _cilCacheProvider.Set(key, cachedValue, region);
+                        Store(key, region, cachedValue, dependencies);
+                        return cachedValue;
+                    }
+                }
+                finally
+                {
+                    // We don't need the lock anymore
+                    object tempKeyLock;
+                    KeyLocks.TryRemove(hash, out tempKeyLock);
+                }
+            }
+            return default(T);
+        }
+
+        private bool TryGetCachedValue<T>(string key, string region, out T value)
+        {
+            var cachedValue = _cilCacheProvider.Get(key, region);
             if (cachedValue != null)
             {
-                value = cachedValue;
+                if (!(cachedValue is T))
+                {
+                    throw new DxaException(
+                        $"Cached value for key '{key}' in region '{region}' is of type {cachedValue.GetType().FullName} instead of {typeof(T).FullName}."
+                        );
+                }
+                value = (T)cachedValue;
                 return true;
             }
-
             value = default(T);
             return false;
         }
 
-        public T GetOrAdd<T>(string key, string region, Func<T> addFunction, IEnumerable<string> dependencies = null)
-        {
-            var cachedValue = GetCachedValue<T>(key, region);
-
-            if (cachedValue == null)
-            {
-                var hash = CalcHashKey(key, region);
-
-                lock (KeyLocks.GetOrAdd(hash, new object()))
-                {
-                    try
-                    {
-                        // Try and get from cache again in case it has been added in the meantime
-                        cachedValue = GetCachedValue<T>(key, region);
-
-                        // Still null, so lets run Func()
-                        if (cachedValue == null && (cachedValue = addFunction()) != null)
-                        {
-                            // Note that dependencies are not used?
-                            Debug.Assert(_cilCacheProvider != null, "_cilCacheProvider != null");
-                            _cilCacheProvider.Set(key, cachedValue, region);
-                            Store(key, region, cachedValue, dependencies);
-                        }
-                    }
-                    finally
-                    {
-                        // We don't need the lock anymore
-                        object tempKeyLock;
-                        KeyLocks.TryRemove(hash, out tempKeyLock);
-                    }
-                }
-            }
-
-            return cachedValue != null ? cachedValue : default(T);
-        }
-
-        private T GetCachedValue<T>(string key, string region)
-        {
-            var cachedValue = _cilCacheProvider.Get(key, region);
-
-            return cachedValue == null ? default(T) : (T)cachedValue;
-        }
-
-        private static string CalcHashKey(string key, string region)
-        {
-            return $"{region}:{key}";
-        }
+        private static string CalcHashKey(string key, string region) => $"{region}:{key}";
     }
 }
