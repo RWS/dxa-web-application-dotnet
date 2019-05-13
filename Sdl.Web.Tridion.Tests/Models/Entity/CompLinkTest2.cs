ed3b22cde5815d0a05fa28ad7fc6abce2f60aedf
GLOBAL\padams <padams@sdl.com>
2019-05-13 15:40:59 +0100

    CRQ-14184 : Missing file added :)

---
+++ Sdl.Web.Tridion.Tests/Models/Entity/CompLinkTest2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Tridion.Tests.Models
+{
+    public class CompLinkTest2 : EntityModel
+    {
+        [SemanticProperty("compLink")]
+        public List<Link> CompLink { get; set; }
+    }
+}
